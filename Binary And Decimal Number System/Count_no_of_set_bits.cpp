e1e43bebee1f3a3144dbe37afd6a5b796e21b5cc
Abhishek Kadam <kadamabhishek037@gmail.com>
2023-02-19 13:52:14 +0530

    Add files via upload

---
+++ Binary And Decimal Number System/Count_no_of_set_bits.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the value:"<<endl;
+    cin>>n;
+    int ans=0;
+
+    while(n != 0){
+        if(n & 1){
+            ans++;
+        }
+        n = n >> 1;
+    }
+    cout<<"the set bits "<< "are "<<ans;
+}
