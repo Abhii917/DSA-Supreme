5a25245e3c44ac7fc21a7d8de1ea01e2b73b6af3
Abhishek Kadam <kadamabhishek037@gmail.com>
2023-02-28 17:14:15 +0530

    Add files via upload

---
+++ Basic/print_primes_1_to_n.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+bool isPrime(int n){
+    int i = 2;
+    for ( i = 2; i < n; i++)
+    {
+        if(n % i == 0){
+            return false;
+        }
+        return true;
+    }
+    
+}
+
+int main(){
+   int n;
+   cout<<"Enter a number:"<<endl;
+   cin>>n;
+
+   bool ans = isPrime(n);
+
+
+    for (int i = 2; i <=n; i++)
+    {
+        bool ans = isPrime(i);
+        if(ans){
+            cout<<i<<" ";
+        }
+    }
+    
+}
